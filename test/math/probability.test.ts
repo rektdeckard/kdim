83d02d9bb862831cadcb7ebe9e1a033096c48454
rektdeckard <friedtm@gmail.com>
2023-08-12 16:26:22 -0600

    feat(probability): add tests

--- test/math/probability.test.ts
+++ test/math/probability.test.ts
@@ -1,5 +1,37 @@
 import { describe, it, expect } from "vitest";
-import { Probability, ProbabilityEvent } from "../../src";
+import { Probability, ProbabilityEvent, Range } from "../../src";
+
+function countSamples<T>(
+  samples: Array<ProbabilityEvent<T>>,
+  idFn: (value: T) => string
+): Record<string, number> {
+  return samples.reduce<Record<string, number>>((acc, curr) => {
+    const id = idFn(curr.value);
+    acc[id] = (acc[id] ?? 0) + 1;
+    return acc;
+  }, {});
+}
+
+function mermaid<T>(
+  title: string,
+  samples: Array<ProbabilityEvent<T>>,
+  idFn: (value: T) => string
+): string {
+  const sampleCounts = countSamples(samples, idFn);
+
+  let markup = `\
+pie title ${title}
+${Object.entries(sampleCounts)
+  .map(([id, count]) => `    "${id}": ${count}`)
+  .join("\n")}
+  `;
+
+  return markup;
+}
+
+function variance(actual: number, expected: number, total: number): number {
+  return Math.abs((actual - expected) / expected);
+}
 
 describe("Probability", () => {
   describe("new Probability", () => {
@@ -22,6 +54,27 @@ describe("Probability", () => {
 
       const d = new Probability(e1, e2, e3);
       expect(d.sample()).toBe(e1);
+    });
+  });
+
+  describe("distributions", () => {
+    it("produces a believable distribution for large sample size", () => {
+      const n = 10000;
+      const p = new Probability(
+        { value: "Dog", p: 0.5 },
+        { value: "Cat", p: 0.25 },
+        { value: "Snake", p: 0.125 },
+        { value: "Chinchilla", p: 0.125 }
+      );
+
+      const samples = Range.of(n).map(() => p.sample());
+      const counts = countSamples(samples, (it) => it);
+      expect(variance(counts["Dog"], n * 0.5, n)).toBeLessThan(0.15);
+      expect(variance(counts["Cat"], n * 0.25, n)).toBeLessThan(0.15);
+      expect(variance(counts["Snake"], n * 0.125, n)).toBeLessThan(0.15);
+      expect(variance(counts["Chinchilla"], n * 0.125, n)).toBeLessThan(0.15);
+
+      console.log(mermaid("Pets", samples, (it) => it));
     });
   });
 });
